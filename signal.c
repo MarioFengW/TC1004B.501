4fac968c5832735b9a7934880acdaff48b278e57
MarioFengW <88916037+MarioFengW@users.noreply.github.com>
2024-11-20 14:06:58 +0000

    Codded signal

--- signal.c
+++ signal.c
@@ -3,34 +3,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-volatile sig_atomic_t stop = 0; 
+volatile sig_atomic_t stop = 0;
 
-void no_me_matas(int signum) {
+void no_me_matas(int signum)
+{
 
     printf("Recibi la señal %d\n", signum);
     printf("No me puedes matar\n");
-
 }
 
-void me_matas(int signum) {
+void me_matas(int signum)
+{
 
     printf("Recibi la señal %d\n", signum);
     printf("Me mataron\n");
 
-    stop = 1; 
+    stop = 1;
 }
 
-int main() {
+int main()
+{
 
-    signal(2, no_me_matas); 
-    signal(10, me_matas);  
+    signal(2, no_me_matas);
+    signal(10, me_matas);
 
-    while (stop==0) {  
+    while (stop == 0)
+    {
         printf("Trabajando...\n");
         sleep(1);
     }
 
     printf("Fin del programa.\n");
-    
+
     return 0;
 }
